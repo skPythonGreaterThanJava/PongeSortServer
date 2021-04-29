3d2ec92ef8ca3d9590c7e679e49384e64fd1f845
sesana.federico <sesana.federico@TAVE-PC05.OSDBSESTO.local>
2021-04-29 14:06:59 +0200

    Research part1

--- PongeSortServer/src/client/Client.java
+++ PongeSortServer/src/client/Client.java
@@ -6,9 +6,13 @@
 package client;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,21 +20,35 @@ import java.net.Socket;
  */
 public class Client {
     
-    public static void main(String args[]) throws IOException {
-        Socket server = new Socket("localhost", 42069);
-        char[] list = new char[10];
-        for(int i = 0; i < 10; i++) 
-            list[i] = (char) (int) (Math.random()*23 + 65);
-        
-        OutputStream toServer = server.getOutputStream();
-        ObjectOutputStream out = new ObjectOutputStream(toServer);
-        
-        out.writeObject(list);
-        
-        toServer.flush();
-        toServer.close();
-        out.flush();
-        out.close();
+    public static void main(String args[]) {
+        try {
+            Socket server = new Socket("localhost", 42069);
+            char[] list = new char[10];
+            for(int i = 0; i < 10; i++)
+                list[i] = (char) (int) (Math.random()*23 + 65);
+            
+            OutputStream toServer = server.getOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(toServer);
+            
+            out.writeObject(list);
+            
+            InputStream fromServer = server.getInputStream();
+            ObjectInputStream in = new ObjectInputStream(fromServer);
+            
+            char[] randls = (char[]) in.readObject();
+            
+            fromServer.close();
+            in.close();
+            
+            toServer.flush();
+            toServer.close();
+            out.flush();
+            out.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         
     }
--- PongeSortServer/src/server/Server.java
+++ PongeSortServer/src/server/Server.java
@@ -8,6 +8,8 @@ package server;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -28,7 +30,33 @@ public class Server {
             ObjectInputStream in = new ObjectInputStream(fromClient);
             
             char[] list = (char[]) in.readObject();
-            System.out.println(list[0]);
+            System.out.println(list);
+            
+            char[] randls = new char[10]; 
+            
+            for(int i = 0; i < 10; i++){
+                int j = 0;
+                while(true) {
+                    j = (int) Math.random()*9;
+                    if(list[j] != -1)
+                        break;
+                }
+                
+                randls[i] = list[j];
+            }
+            
+            OutputStream toClient = client.getOutputStream();
+            ObjectOutputStream out = new ObjectOutputStream(toClient);
+            
+            out.writeObject(randls);
+            
+            toClient.flush();
+            out.flush();
+            toClient.close();
+            out.close();
+            
+            fromClient.close();
+            in.close();
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
